27f5408647a0f254f4c8e92ff42bb76deab71c23
Laptopia <verbatim4eg@gmail.com>
2026-09-07 23:36:08 +0300

    Add child theme directories and enqueue Laptopia stylesheet

--- functions.php
+++ functions.php
@@ -8,7 +8,7 @@ add_action( 'wp_enqueue_scripts', function() {
 
     wp_enqueue_style(
         'laptopia-child',
-        get_stylesheet_uri(),
+        get_stylesheet_directory_uri() . '/assets/css/laptopia.css',
         array( 'hello-elementor-parent' ),
         wp_get_theme()->get( 'Version' )
     );
