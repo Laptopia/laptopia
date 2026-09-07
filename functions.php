844695c5326178861656ad40a16e4f7c4946bc14
Laptopia <verbatim4eg@gmail.com>
2026-09-08 00:18:38 +0300

    Make Laptopia a standalone WordPress theme

--- functions.php
+++ functions.php
@@ -6,14 +6,9 @@ add_action( 'after_setup_theme', function() {
 
 add_action( 'wp_enqueue_scripts', function() {
     wp_enqueue_style(
-        'hello-elementor-parent',
-        get_template_directory_uri() . '/style.css'
-    );
-
-    wp_enqueue_style(
         'laptopia-child',
         get_stylesheet_directory_uri() . '/assets/css/laptopia.css',
-        array( 'hello-elementor-parent' ),
+        array(),
         wp_get_theme()->get( 'Version' )
     );
 } );
