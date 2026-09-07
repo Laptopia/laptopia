e028d1e93418743bf115e5e6638f5949b409a820
Laptopia <verbatim4eg@gmail.com>
2026-09-08 00:01:58 +0300

    Add HTML document structure and WordPress hooks to homepage template

--- page-templates/home-laptopia.php
+++ page-templates/home-laptopia.php
@@ -6,6 +6,18 @@
 defined( 'ABSPATH' ) || exit;
 
 
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+  <meta charset="<?php bloginfo( 'charset' ); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+  <?php wp_body_open(); ?>
+
+<?php
 get_template_part( 'template-parts/header' );
 get_template_part( 'template-parts/hero' );
 get_template_part( 'template-parts/services' );
@@ -21,3 +33,7 @@ get_template_part( 'template-parts/contact' );
 get_template_part( 'template-parts/footer' );
 get_template_part( 'template-parts/floating-whatsapp' );
 
+?>
+  <?php wp_footer(); ?>
+</body>
+</html>
