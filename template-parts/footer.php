939a0129bf6b468666e255211c2f3ca2b2f7fac7
Dmitrii Milovidov <verbatim4eg@gmail.com>
2026-09-11 00:46:41 +0300

    Add compact service navigation to footer

--- template-parts/footer.php
+++ template-parts/footer.php
@@ -1,3 +1,8 @@
 <footer class="laptopia-footer">
   Laptopia – מעבדת מחשבים ניידים ברמלה
+  <nav class="laptopia-footer-services" aria-label="שירותי המעבדה">
+    <a href="https://laptopia.co.il/motherboard-repair/">תיקון לוח אם למחשב נייד</a>
+    <a href="https://laptopia.co.il/screen-replacement/">החלפת מסך למחשב נייד</a>
+    <a href="https://laptopia.co.il/battery-replacement/">החלפת סוללה למחשב נייד</a>
+  </nav>
 </footer>
