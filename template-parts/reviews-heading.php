9fd0ad2f2a85e02234317e76f26f71728cefe7ef
Dmitrii Milovidov <verbatim4eg@gmail.com>
2026-09-12 00:27:43 +0300

    Add Google review button to homepage

--- template-parts/reviews-heading.php
+++ template-parts/reviews-heading.php
@@ -10,6 +10,10 @@
       ביקורות אמיתיות מ-Google
     </div>
 
+    <div class="laptopia-buttons">
+      <a class="laptopia-btn laptopia-btn-dark" href="https://g.page/r/Ccprx_3WhBpaEAE/review" target="_blank" rel="noopener noreferrer">כתבו ביקורת בגוגל</a>
+    </div>
+
   </div>
 
 </section>
